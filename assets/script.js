8e2e600dd74e29b715d0d5c9f42b4f6573a8af6a
AlterrasNex <nicolasbrand@outlook.com>
2025-09-17 22:57:35 +0200

    Portfolio imported on github :tada:

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+function hiddenUp() {
+    document.querySelector(".hidden-down").classList.add("hidden-up");
+    document.querySelector(".content-hidden").classList.remove("content-hidden");
+    document.querySelector(".content-hidden").classList.add("content");
+}
